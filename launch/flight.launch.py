5debc7675943a9decbd249d6ecbac7567a5f9d31
Eric Slaghuis <eslaghuis@gmail.com>
2021-10-16 10:22:55 +0200

    Added parameters to navigation BT

--- launch/flight.launch.py
+++ launch/flight.launch.py
@@ -2,16 +2,18 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package="flight_control",
-            executable="flight_control_node",
-            name="flight_control_node",
-            output="screen",
-            emulate_tty=True,
-            parameters=[
-                {"mission_bt_file": "./src/flight_control/behaviour_trees/sample.xml"},
-                {"minimum_battery_voltage": 13.6}
-            ]
-        )
-    ])
+    ld = LaunchDescription()
+
+    flight_control_node = Node(
+        package="flight_control",
+        executable="flight_control_node",
+        parameters=[
+            {"mission_bt_file":    "./src/flight_control/behaviour_trees/sample.xml"},
+            {"navigation_bt_file": "./src/navigation_lite/behavior_trees/navigate.xml"},            
+            {"minimum_battery_voltage": 13.6}
+        ]
+    )
+
+    ld.add_action(flight_control_node)
+
+    return ld
