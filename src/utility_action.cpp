588c2d01a79a23257a448f9de9464fd23997354e
Eric Slaghuis <eslaghuis@gmail.com>
2021-10-29 16:52:53 +0200

    Code Refresh

--- src/utility_action.cpp
+++ src/utility_action.cpp
@@ -1,4 +1,5 @@
 #include "flight_control/utility_action.h"
+#include "rclcpp/rclcpp.hpp"
 
 namespace DroneNodes
 {
@@ -11,7 +12,7 @@ BT::NodeStatus SaySomething::tick()
         throw BT::RuntimeError( "missing required input [message]: ", msg.error() );
     }
 
-    std::cout << "Robot says: " << msg.value() << std::endl;
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Robot says: " + msg.value());
     return BT::NodeStatus::SUCCESS;
 }
   
