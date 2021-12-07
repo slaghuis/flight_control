f028ded9898c362188d8ecacf42ed0d52f9b4ab3
Eric Slaghuis <eslaghuis@gmail.com>
2021-12-07 09:55:11 +0200

    Add files via upload

---
+++ include/flight_control/camera_save_action.h
@@ -0,0 +1,54 @@
+#ifndef PICTURE_ACTION_H
+#define PICTURE_ACTION_H
+
+#include <functional>
+#include <future>
+#include <memory>
+#include <string>
+#include <chrono>
+#include <cstdlib>
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/client.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+
+#include "behaviortree_cpp_v3/behavior_tree.h"
+#include "behaviortree_cpp_v3/bt_factory.h"
+
+#include "camera_lite_interfaces/srv/save.hpp"
+
+namespace DroneNodes
+{
+
+class CameraSaveAction : public BT::AsyncActionNode
+{
+  public:
+
+    CameraSaveAction(const std::string& name, const BT::NodeConfiguration& config)
+      : BT::AsyncActionNode(name, config)
+    { }
+
+    void init(rclcpp::Node::SharedPtr node) {
+      node_ = node;   
+      this->client_ptr_ = node_->create_client<camera_lite_interfaces::srv::Save>("camera/save_picture");    
+    }
+    
+    static BT::PortsList providedPorts()
+    {
+        return{ BT::InputPort<std::string>("filename") };
+    }
+    
+    BT::NodeStatus tick() override;
+    void halt() override;
+  private:
+    std::atomic_bool _halt_requested;
+    
+    // Pointer to the ROS node
+    rclcpp::Node::SharedPtr node_;
+    
+    rclcpp::Client<camera_lite_interfaces::srv::Save>::SharedPtr client_ptr_;
+};
+     
+} // Namespace
+
+#endif // PICTURE_ACTION_H
--- include/flight_control/utility_action.h
+++ include/flight_control/utility_action.h
@@ -4,6 +4,13 @@
 #include "behaviortree_cpp_v3/behavior_tree.h"
 #include "behaviortree_cpp_v3/bt_factory.h"
 
+#include "rclcpp/rclcpp.hpp"
+
+#include <time.h>
+
+#define FILENAME_FORMAT "%Y%m%d_%H%M%S.jpg"
+#define FILENAME_SIZE 20
+
 namespace DroneNodes
 {
      
@@ -24,6 +31,25 @@ class SaySomething : public BT::SyncActionNode
         return{ BT::InputPort<std::string>("message") };
     }
 };  
+
+
+class GenerateFilename : public BT::SyncActionNode
+{
+  public:
+    GenerateFilename(const std::string& name, const BT::NodeConfiguration& config)
+      : BT::SyncActionNode(name, config)
+    {
+    }
+
+    // You must override the virtual function tick()
+    BT::NodeStatus tick() override;
+
+    static BT::PortsList providedPorts()
+    {
+        return { BT::OutputPort<std::string>("filename") };
+    }
+
+};
   
 } // Namespace
 
